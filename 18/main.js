ba1984308392a2dfdbd52905d74be9519eaff040
C-Jansek <mailcjanssen@gmail.com>
2020-12-18 07:33:07 +0100

    Day 18 refactor

--- 18/main.js
+++ 18/main.js
@@ -1,54 +1,64 @@
 const fs = require("fs");
-const _ = require("lodash");
-
 const expressions = fs.readFileSync("input.csv", "utf8").split("\n");
-let allExpressionsSum = 0;
-expressions.forEach((expression) => {
-    const paranthesesRegex = /\([\s\+\*\d]+\)/;
-    // console.log("input:", expression);
-    while (expression.match(paranthesesRegex)) {
-        replaceVal = evaluateExpression(
-            expression.match(paranthesesRegex)[0].slice(1, -1)
-        );
-        expression = expression.replace(paranthesesRegex, replaceVal);
-    }
-    expression = evaluateExpression(expression);
-    allExpressionsSum += expression;
-    // console.log("done:", expression);
-});
-console.log("Part One:", allExpressionsSum);
 
-function evaluateExpression(expression, additionFirst = false) {
+console.log("Part One:", processExpressions(expressions));
+
+console.log("Part Two:", processExpressions(expressions, true));
+
+
+/**
+ * Evaluate all expressions, operations within parantheses first.
+ * However, this is not using the general mathematical precedence levels,
+ * but by evaluating scrictly left to right, or with reversed precedence.
+ * @param {string[]} expressions array with the expressions that should be evaluated
+ * @param {boolean} [prioritizePlus = false] when true, evaluate addition before multiplication. 
+ * @return {number} sum of all the evaluated expressions
+ */
+function processExpressions(expressions, prioritizePlus = false) {
+    let allExpressionsSum = 0;
+    const paranthesesRegex = /\([\s\+\*\d]+\)/;
+    expressions.forEach((expression) => {
+        // Surround addition operation with parantheses such that it will be prioritized
+        if (prioritizePlus) {
+            expression = expression.replace(/(\d+\s\+\s\d+)/, `($1)`);
+        }
+        while (expression.match(paranthesesRegex)) {
+            // Process instruction between parantheses first
+            replaceVal = evaluateExpression(
+                expression.match(paranthesesRegex)[0].slice(1, -1)
+            );
+            // and replace its returned value in instruction
+            expression = expression.replace(paranthesesRegex, replaceVal);
+            // Surround addition operation with parantheses such that it will be prioritized
+            if (prioritizePlus) {
+                expression = expression.replace(/(\d+\s\+\s\d+)/, `($1)`);
+            }
+        }
+        // Process the final expression
+        expression = evaluateExpression(expression);
+        // Add it to the total
+        allExpressionsSum += Number(expression);
+    });
+    return allExpressionsSum;
+}
+
+/**
+ * Evaluate this single expression
+ * @param {string} expression expression to be evaluated
+ * @return {number} the value the expression evaluates to
+ */
+function evaluateExpression(expression) {
     expression = expression.split(" ");
     let nextOperation;
-    console.log("before:", expression);
     expression = expression.reduce((acc, cur) => {
+        // Set next operation ready
         if (cur === "*" || cur === "+") {
             nextOperation = cur;
             return acc;
         }
+        // Do operation
         if (nextOperation === "*") return Number(acc) * Number(cur);
         if (nextOperation === "+") return Number(acc) + Number(cur);
     });
-    console.log("reduced:", expression);
     return expression;
 }
-
-allExpressionsSum = 0;
-expressions.forEach((expression) => {
-    const paranthesesRegex = /\([\s\+\*\d]+\)/;
-    console.log("input:", expression);
-    expression = expression.replace(/(\d+)(\s\+\s\d+)/, `($1$2)`);
-    while (expression.match(paranthesesRegex)) {
-        console.log('do',expression)
-        replaceVal = evaluateExpression(
-            expression.match(paranthesesRegex)[0].slice(1, -1)
-        );
-        expression = expression.replace(paranthesesRegex, replaceVal);
-        expression = expression.replace(/(\d+)((\s\+\s\d+)+)/, `($1$2)`);
-    }
-    expression = evaluateExpression(expression);
-    allExpressionsSum += Number(expression);
-    console.log("done:", expression);
-});
-console.log("Part Two:", allExpressionsSum);
